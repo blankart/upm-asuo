a59036f8f18aa1dbe684a9d09adc8af18626dd7f
Nico Mendoza <blankartwp@gmail.com>
2018-04-25 10:18:52 +0800

    Added 3 php function under subfolder org

---
+++ application/views/org/applyforaccreditation.php
@@ -0,0 +1,18 @@
+<div class="modal animated bounceInUp" id="applyforaccreditation" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" style="min-width: 1200px;">
+        <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Title</h4>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body">
+
+                </div>
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
