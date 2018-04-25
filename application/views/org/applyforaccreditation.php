017b7a63afec03cbfd4e56230edc845cbdf45ee3
Nico Mendoza <blankartwp@gmail.com>
2018-04-26 00:35:45 +0800

    [VIEW] Added contents under applyforaccreditation.php (UI)

--- application/views/org/applyforaccreditation.php
+++ application/views/org/applyforaccreditation.php
@@ -1,18 +1,52 @@
-<div class="modal animated bounceInUp" id="applyforaccreditation" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-dialog-centered" style="min-width: 1200px;">
-        <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <h4 class="modal-title">Title</h4>
-            </div>
-            <!-- Modal body -->
-            <div class="modal-body">
+<div class="modal animated bounceInUp" id="applyforaccreditation">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
 
-                </div>
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                </div>
-            </div>
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Accreditation Requirements</h4>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <table>
+          <tr>
+            <td>Accreditation Application</td>
+            <td width="500" align="right"><button type="button" value="formA" class="btn btn-info" data-toggle="modal" data-target="#formA">Edit</button></td>          
+          </tr>
+          <tr>
+            <td>Consent of Adviser</td>
+            <td align="right"><button type="button" value="formB" class="btn btn-info" data-toggle="modal" data-target="#formB">Edit</button></td>
+          </tr>
+          <tr>
+            <td>Organization Profile</td>
+            <td align="right"><button type="button" value="formC" class="btn btn-info" data-toggle="modal" data-target="#formC">Edit</button></td>
+          </tr>
+          <tr>
+            <td>Officers' Profile</td>
+            <td align="right"><button type="button" value="formD" class="btn btn-info" data-toggle="modal" data-target="#formD">Edit</button></td>
+          </tr>
+          <tr>
+            <td>Members' Profile</td>
+            <td align="right"><button type="button" value="formE" class="btn btn-info" data-toggle="modal" data-target="#formE">Edit</button></td>
+          </tr>
+          <tr>
+            <td>Financial Report</td>
+            <td align="right"><button type="button" value="formF" class="btn btn-info" data-toggle="modal" data-target="#formF">Edit</button></td>
+          </tr>
+        </table>
+
+        <!-- progress bars -->
+        <div class="progress">
+          <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">70% Complete (danger)</div>
         </div>
+        
+      </div>
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
     </div>
+  </div>
+</div>
