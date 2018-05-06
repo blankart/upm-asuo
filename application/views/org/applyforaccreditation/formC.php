27ac2dd1b68a8377480c9ece973d7b55d9e68526
Ma. Christina J. Galvez <tintingalvez@gmail.com>
2018-05-06 11:19:59 +0800

    [View] edited form A and C

--- application/views/org/applyforaccreditation/formC.php
+++ application/views/org/applyforaccreditation/formC.php
@@ -17,24 +17,22 @@
 		<script src="<?php echo base_url();?>js/script.js"></script>
 	</head>
 
-<body>
-   <div class="animated fadeIn" style="background-color: rgb(255,255,255); margin-top: 60px; padding-top: 50px; box-shadow: 0 0 40px rgba(0,0,0,.50); padding-bottom: 500px;">
-        <!-- div class="container" -->
-            <!-- div class="row" -->
-                <div class="col-4">
-                    <!-- insert sidenav -->
-                    <div class="sidenav">
-                        <li><a href="<?php echo base_url(); ?>org/applyforaccreditation">Home</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formA">Accreditation Application</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formB">Consent of Adviser</a></li>
-                        <li class="active"><a href="<?php echo base_url(); ?>org/formC">Organization Profile</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formD">Officers' Profile</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formE">Members' Profile</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formF">Projects</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formG">Financial Report</a></li>
-                    </div>
+	<body>
+   		<div class="animated fadeIn" style="background-color: rgb(255,255,255); margin-top: 60px; padding-top: 50px; box-shadow: 0 0 40px rgba(0,0,0,.50); padding-bottom: 500px;">
+            <div class="col-4">
+                <!-- insert sidenav -->
+                <div class="sidenav">
+                    <li><a href="<?php echo base_url(); ?>org/applyforaccreditation">Home</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formA">Accreditation Application</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formB">Consent of Adviser</a></li>
+                    <li class="active"><a href="<?php echo base_url(); ?>org/formC">Organization Profile</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formD">Officers' Profile</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formE">Members' Profile</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formF">Projects</a></li>
+                    <li><a href="<?php echo base_url(); ?>org/formG">Financial Report</a></li>
                 </div>
-                    <!-- Page Content -->
+            </div>
+        <!-- Page Content -->
 		<div class="main">
 			<h1>Form C: Organization Profile</h1>
 			<!-- progress bar -->
@@ -43,17 +41,19 @@
 			<!-- form -->
 			<form id="multiphase2" onsubmit="return false">
 				<div id="cphase1">
-					Name of Organization: <input type="text" name="orgName" value="<?php echo $row['org_name']; ?>" disabled><br>
-					Acronym: <input type="text" name="acronym" value="<?php echo $row['acronym']; ?>" disabled><br>
-					Mailing Address: <input type="text" name="address"><br>
-					Email Address: <input type="text" name="email" disabled><br>
-					Website: <input type="text" name="website" value="<?php echo $row['org_website']; ?>" disabled><br> 
-					Date Established: <input type="text" name="established" value="<?php echo $row['date_established']; ?>" disabled><br><br>
+					Name of Organization:&nbsp;&nbsp;<input type="text" id="orgName" name="orgName" value="<?php echo $row['org_name']; ?>" disabled>&nbsp;&nbsp;&nbsp;
+					Acronym:&nbsp;&nbsp;<input type="text" id="acronym" name="acronym" value="<?php echo $row['acronym']; ?>" disabled>
+					Mailing Address:&nbsp;&nbsp;<input type="text" id="address" name="address">
+					Email Address:&nbsp;&nbsp;<input type="text" id="email" name="email" disabled>&nbsp;&nbsp;
+					Website:&nbsp;&nbsp;<input type="text" id="website" name="website" value="<?php echo $row['org_website']; ?>" disabled>&nbsp;&nbsp;
+					Date Established:&nbsp;&nbsp;<input type="text" id="established" name="established" value="<?php echo $row['date_established']; ?>" disabled>
+					<br><br>
+
 					<button class="button" onclick="processcPhase1()">Continue</button>
 				</div>
 
 				<div id="cphase2">
-					Total Number of Members: <input type="text" name="numMembers" disabled><br><br>
+					Total Number of Members:&nbsp;&nbsp;<input type="text" id="numMembers" name="numMembers" disabled><br><br>
 					<!--
 					<table>
 						<tr>
@@ -92,9 +92,10 @@
 			
 				<div id="cphase3">
 					Is your organization incorporated with the Securities and Exchange Commission(SEC)?<br>
-					<input type="radio" name="no" value="no">No<br>
-					<input type="radio" name="yes" value="yes">Yes, when?
-					<input type="text" name="when"><br><br>
+					<input type="radio" id="no" name="no" value="no">&nbsp; No &nbsp;&nbsp;
+					<input type="radio" id="yes" name="yes" value="yes">&nbsp; Yes,&nbsp;when?&nbsp;&nbsp;
+					<input type="text" id="when" name="when" placeholder="year">
+					<br><br>
 
 					<button class="button" onclick="processcPhase3()">Continue</button>
 					<button class="button" onclick="backc2()">Back</button>
