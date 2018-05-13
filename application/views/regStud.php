4ad25be7ad717d3f4b83fb35e5b7c0f817ae85b2
ZENBOOK\Mam Vangie <jejeayos@gmail.com>
2018-05-13 16:42:00 +0800

    [V]: Added JavaScript to regStud.php

--- application/views/regStud.php
+++ application/views/regStud.php
@@ -1,7 +1,143 @@
 <script>
-	
+	function UPMailChecker(){
+        var up_mail = ($("#up_mail").val()).trim();
+        var checker = false;
+
+    if (up_mail.length>0)
+    {
+        //AJAX IF EXISTING YUNG EMAIL
+        //alert(org_email);
+        $.ajax({
+       type:"post",
+       url:"",
+       cache: false,
+       data:{up_mail: up_mail},
+       dataType: 'json',
+       async: false,
+       success:function(result)
+       {
+            //alert(JSON.stringify (result));
+            if(result == true)
+            {
+                $("#UPMailTaken").removeClass();
+                $("#UPMailTaken").html('');
+                $("#UPMailTaken").addClass('notice notice-sm notice-danger');
+                $("#UPMailTaken").html('<strong>Error:</strong> This email address is already linked to another account. For more info, visit OSA.');
+                $("#UPMailTaken").slideDown(400);   
+                return false;
+            }
+            else if(result == false)
+            {
+               // alert("Abc");
+                $("#UPMailTaken").fadeOut(400);
+                checker = true;
+                return true;
+            }
+            
+       }
+
+    });
+
+    }
+    return checker;
+    
+}
+
+	function conPassValidate()
+{
+    var conPass = $("#conPass").val();
+    var Pass = $("#Pass").val();
+    if (conPass!=Pass && $("#conPass").val().length > 0 && $("#Pass").val().length > 0){
+        $("#studPwChecker").removeClass();
+        $("#studPwChecker").html('');
+        $("#studPwChecker").addClass('notice notice-sm notice-danger');
+        $("#studPwChecker").html('<strong>Error:</strong> Password does not match.');
+        $("#studPwChecker").slideDown(400);
+        return false;
+    }
+    else if(conPass==Pass && $("#conPass").val().length > 0 && $("#Pass").val().length > 0){
+        //alert("tasdasd");
+        $("#studPwChecker").fadeOut(400);
+        return true;
+    }
 
 }
+
+function validateForm(){
+    resetErrorDisplays();
+
+    if (conPassValidate())
+    {
+        
+        var first_name = (document.getElementById("first_name").value).trim();
+        var middle_name = (document.getElementById("middle_name").value).trim();
+        var last_name = (document.getElementById("last_name").value).trim();
+        var up_id = (document.getElementById("up_id").value).trim();
+        var contact_num = (document.getElementById("contact_num").value).trim();
+        var mailing_address = (document.getElementById("mailing_address").value).trim();
+
+
+        if(first_name == '' || middle_name == '' || last_name == '' || up_id == '' || contact_num == '' || mailing_address == ''){
+
+            if(first_name == '' || middle_name == '' || last_name == '' ){
+                $("#nameInvalidInput").addClass('notice notice-sm notice-danger');
+                $("#nameInvalidInput").html('<strong>Error:</strong> Organizaton Name field is empty!');
+                $("#nameInvalidInput").slideDown(400);   
+            }
+
+            if(up_id == ""){
+                $("#StudNumInvalidInput").addClass('notice notice-sm notice-danger');
+                $("#StudNumInvalidInput").html('<strong>Error:</strong> Organization Website field is empty!');
+                $("#StudNumInvalidInput").slideDown(400);  
+            }
+
+            if(contact_num == ""){
+                $("#NumInvalidInput").addClass('notice notice-sm notice-danger');
+                $("#NumInvalidInput").html('<strong>Error:</strong> Organization Website field is empty!');
+                $("#NumInvalidInput").slideDown(400);  
+            }
+    
+            if(mailing_address == ""){
+                $("#mailAddressInvalidInput").addClass('notice notice-sm notice-danger');
+                $("#mailAddressInvalidInput").html('<strong>Error:</strong> Mailing Address field is empty!');
+                $("#mailAddressInvalidInput").slideDown(400);   
+            }
+
+            return false;
+        }
+        else{
+            document.getElementById("first_name").value = first_name;
+            document.getElementById("middle_name").value = middle_name;
+            document.getElementById("last_name").value = last_name;
+            document.getElementById("up_id").value = up_id;
+            document.getElementById("up_mail").value = up_mail;
+            document.getElementById("contact_num").value = contact_num;
+            document.getElementById("mailing_address").value = mailing_address;
+
+            return true;
+        }
+    }
+    else {
+        return false;
+    };
+}
+
+    function resetErrorDisplays(){
+        $("#nameInvalidInput").removeClass();
+        $("#nameInvalidInput").html('');
+
+        $("#UPMailTaken").removeClass();
+        $("#UPMailTaken").html('');
+
+        $("#mailAddressInvalidInput").removeClass();
+        $("#mailAddressInvalidInput").html('');
+
+        $("#StudNumInvalidInput").removeClass();
+        $("#StudNumInvalidInput").html('');
+
+        $("#NumInvalidInput").removeClass();
+        $("#NumInvalidInput").html('');        
+    }
 </script>
 <!DOCTYPE html>
 <html>
@@ -45,7 +181,11 @@
                            </div>
                            <div class="form-group">
                               <div class="row">
-                                 <div class="col-6">
+                                 <div class="col-3">
+                                    <input type="text" maxlength="30" class="form-control" placeholder="Student Number" name="data[up_id]" id='up_id' required>
+                                    <small class="form-text text-muted">20XXXXXXX</small>
+                                 </div>
+                                 <div class="col-3">
                                     <select class="form-control" name="data[year_level]" required>
                                        <option value ='1' >First Year</option>
                                        <option value ='2' >Second Year</option>
@@ -85,6 +225,8 @@
                                     </select>
                                     <small class="form-text text-muted">Sed ut perspiciatis.</small>
                                  </div>
+                                 <div id="StudNumInvalidInput" class="notice notice-sm notice-danger" style="display: none;">
+                              </div>
                               </div>
                            </div>
                            <div class="form-group">
@@ -105,17 +247,17 @@
                            <div class="form-group">
                               <div class="row">
                                  <div class="col-7">
-                                    <input type="email" maxlength="50" onblur="" class="form-control" name="data[up_mail]" id='up_mail' placeholder="UP Mail" required>
+                                    <input type="email" maxlength="50" onblur="UPMailChecker()" class="form-control" name="data[up_mail]" id='up_mail' placeholder="UP Mail" required>
                                     <small class="form-text text-muted">Sed ut perspiciatis.</small>
                                  </div>
                                  <div class="col-5">
                                     <input type="text" maxlength="50" class="form-control" name="data[contact_num]" id ='contact_num' placeholder="Phone Number" required>
                                     <small class="form-text text-muted">Sed ut perspiciatis.</small>
-                                    <div id="websiteInvalidInput" class="notice notice-sm notice-danger" style="display: none;"></div>
+                                    <div id="NumInvalidInput" class="notice notice-sm notice-danger" style="display: none;"></div>
                                  </div>
                               </div>
                            </div>
-                           <div id="orgEmailTaken" class="notice notice-sm notice-danger" style="display: none;"></div>
+                           <div id="UPMailTaken" class="notice notice-sm notice-danger" style="display: none;"></div>
                            <div class="form-group">
                               <div class="row">
                                  <div class="col-12">
@@ -128,11 +270,11 @@
                            <div class="form-group">
                               <div class="row">
                                  <div class="col-6">
-                                    <input type="password" minlength="7" maxlength="32" onblur="" id="Pass" class="form-control" placeholder="Password" name="data[password]" required>
+                                    <input type="password" minlength="7" maxlength="32" onblur="conPassValidate()" id="Pass" class="form-control" placeholder="Password" name="data[password]" required>
                                     <small class="form-text text-muted">Sed ut perspiciatis.</small>
                                  </div>
                                  <div class="col-6   ">
-                                    <input type="password" minlength="7" maxlength="32" onblur="" id="conPass" class="form-control" placeholder="Confirm Password" required>
+                                    <input type="password" minlength="7" maxlength="32" onblur="conPassValidate()" id="conPass" class="form-control" placeholder="Confirm Password" required>
                                     <small class="form-text text-muted">Sed ut perspiciatis.</small>
                                  </div>
                               </div>
