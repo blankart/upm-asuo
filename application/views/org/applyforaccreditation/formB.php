07e3ae3a69a46ff9fd9fca611ce446c8facc003b
Ma. Christina J. Galvez <tintingalvez@gmail.com>
2018-05-19 16:34:56 +0800

    fixed formats

--- application/views/org/applyforaccreditation/formB.php
+++ application/views/org/applyforaccreditation/formB.php
@@ -51,7 +51,7 @@
                         <li><a href="<?php echo base_url(); ?>org/formC">Organization Profile</a></li>
                         <li><a href="<?php echo base_url(); ?>org/formD">Officers' Profile</a></li>
                         <li><a href="<?php echo base_url(); ?>org/formE">Members' Profile</a></li>
-                        <li><a href="<?php echo base_url(); ?>org/formF">Activity Report</li>
+                        <li><a href="<?php echo base_url(); ?>org/formF">Activity Report</a>a</li>
                         <li><a href="<?php echo base_url(); ?>org/formG">Financial Report</a></li>
                         <li><a href="<?php echo base_url(); ?>org/plans">Plans</a></li>
                     </ul>
