f56f15e8194998c99157cc95fd472f5fe902b90e
Darlene Perez <darlenegraceq@gmail.com>
2018-05-20 22:54:19 +0800

    Auto stash before merge of "master" and "origin/master"

--- application/views/admin.php
+++ application/views/admin.php
@@ -18,15 +18,15 @@
                   </div>
                   <div class="card-body" style="padding-top: 0px;">
                      <br><button type="button" onclick="validatestudactSearch()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#validatestudact">
-						<span class="btn-label"><i class="fa fa-check-circle-o fa-2x pull-left"></i></span>Validate Student Account</button></br>
+						<i class="fa fa-check-circle-o fa-2x pull-left"></i>Validate Student Account</button></br>
                      <br><button type="button" onclick="activateorgactSearch()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#activateorgact">
-						<span class="btn-label"><i class="fa fa-check-circle fa-2x pull-left"></i></span>Activate Organization Account</button></br>
+						<i class="fa fa-check-circle fa-2x pull-left"></i>Activate Organization Account</button></br>
                      <br><button type="button" onclick="livesearchallstud()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#viewallstudents">
-						<span class="btn-label"><i class="fa fa-user fa-2x pull-left"></i></span>View All Student Accounts</button></br>
+						<i class="fa fa-user fa-2x pull-left"></i>View All Student Accounts</button></br>
                      <br><button type="button" onclick="livesearchallorg()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#viewallorg">
-						<span class="btn-label"><i class="fa fa-users fa-2x pull-left"></i></span>View All Organization Accounts</button></br>
+						<i class="fa fa-users fa-2x pull-left"></i>View All Organization Accounts</button></br>
                      <br><button type="button" onclick="" class="btn btn-danger admin-button" data-toggle="modal" data-target="#changeloginnotice">
-						<span class="btn-label"><i class="fa fa-edit fa-2x pull-left"></i></span>Change Login Notice</button></br>
+						<i class="fa fa-edit fa-2x pull-left"></i>Change Login Notice</button></br>
                
                   </div>
                </div>
@@ -38,9 +38,13 @@
                   </div>
                   <div class="card-body" style="padding-top: 0px;">
                  <br><button type="button" onclick="searchBox('Pending')" class="btn btn-danger admin-button" data-toggle="modal" data-target="#viewaccreditapp">
-					<span class="btn-label"><i class="fa fa-folder-open fa-2x pull-left"></i></span>View Accreditation Applications</button></br>
+					<i class="fa fa-folder-open fa-2x pull-left"></i>View Accreditation Applications</button></br>
                  <br><button type="button" class="btn btn-danger admin-button" data-toggle="modal" data-target="#openaccreditperiod">
+<<<<<<< Updated upstream
 					<a><i class="fa fa-calendar" style='margin-right: 20px;'></i>Open Accreditation Period</a></button></br>
+=======
+					<i class="fa fa-calendar fa-2x pull-left"></i>Open Accreditation Period</button></br>
+>>>>>>> Stashed changes
                   </div>
                </div>
                <div class="card" style="margin-top: 30px; width: 25rem;">
@@ -49,9 +53,9 @@
                </div>
                   <div class="card-body" style="padding-top: 0px;">
                  <br><button type="button" onclick="searchSendNotice()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#sendnotice">
-						<span class="btn-label"><i class="fa fa-paper-plane-o fa-2x pull-left"></i></span>Send Announcement</button></br>
+						<i class="fas fa-paper-plane pull-left"></i>Send Announcement</button></br>
                     <br><button type="button" onclick="viewAllNotices()" class="btn btn-danger admin-button" data-toggle="modal" data-target="#viewallnotices">
-						<span class="btn-label"><i class="fa fa-envelope-o fa-2x pull-left"></i></span>View All Announcements</button></br>
+						<i class="fa fa-envelope-o fa-2x pull-left"></i>View All Announcements</button></br>
                   </div>
                </div>
             </div>
