22375053b7c77e2877f7510b00d6894147f047c2
Medy Rae J. Fernando <mraejfernando@yahoo.com>
2018-05-20 14:33:09 +0800

    [MVC]: Added resend verification code functionality

--- application/views/errors/html/unverified.php
+++ application/views/errors/html/unverified.php
@@ -1,4 +1,38 @@
 
+<script>
+
+	function resendVerificationCode(){
+
+      $.ajax({
+        type: "POST",
+        url: "<?php echo base_url().'resendVerificationMail'; ?>",
+        cache: false,
+        async: false,
+        dataType: "JSON",
+        data: {source: 'student' },
+        success : function (data){
+        	
+        	if(data){
+	           swal({title: "Success!", text: "A new verification code has been to your email!", type: "success"},
+	           		function(){ 
+	            	location.reload();
+	            	 }
+	            	);
+	       	}
+	       	else{
+	       		swal("Failed!", "You can't resend the verification now. Please try again later.", "error");
+	       	}
+       		},
+                  error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                     //alert("Status: " + textStatus + " | Error: " + errorThrown); 
+                     swal("Failed!", "The system seems to be crashing HAHAHA", "error");
+                  }   
+            });		
+	}
+
+
+</script>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,6 +51,8 @@
 				<h3 class="display-1"><center><i class="fa fa-frown-o fa-3x"></i></h3>
 				<h3 class="display-3"><center>Unverified Account</h3>
 				<p class="lower-case"><center>To verify your account, please follow the link sent to <b><?php echo $org_email; ?></b>.</p>
+				<p class="lower-case"> or </p>
+				<p><button type="button" onclick='resendVerificationCode()' class="btn btn-success">Resend Verification Code</button></p>
 				<a  href="<?php echo base_url().'logout'; ?>"><button type="button" class="btn btn-danger">Go Back</button></a>
 				</div>	
  			</div>
