5a9dd050f9549ac5dc6068716af028ac53d22b35
Ma. Christina J. Galvez <tintingalvez@gmail.com>
2018-05-22 08:04:05 +0800

    [V] edited buttons

--- application/views/org/applyforaccreditation/formB.php
+++ application/views/org/applyforaccreditation/formB.php
@@ -100,7 +100,7 @@
                     <br><br><br>
                  </form>
                  <div class="myBtn">
-                    <button class="btn btn-danger" id="prevBtn" onclick="showPrev()">Preview</button>
+                    <button class="btn btn-danger" id="prevBtn" onclick="showPrev()">Preview</button> 
                  </div>
               </div>
             </div>
